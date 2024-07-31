8d918d33132c0119359212e5f78b6f86df0f82f2
Vitor <vitor-souza.ds@hotmail.com>
2024-07-31 16:24:11 -0300

    Projeto vue

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app2">
+    <h1>Calculadora</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./main/CalculatorM";
+export default {
+  components: { Calculator },
+};
+</script>
+
+<style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+* {
+  font-family: "RobotoMono", monospace;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  background: linear-gradient(to right, rgb(0, 242, 96), rgb(5, 117, 230));
+}
+</style>
